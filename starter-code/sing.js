3affc335259c9a0769b745d66b1d2911ba4c429e
kkuo57 <kkuo507@gmail.com>
2017-02-02 21:26:28 -0800

    first commit

--- starter-code/sing.js
+++ starter-code/sing.js
@@ -1,1 +1,10 @@
 console.log("sing.js loaded");
+
+var bottleNumber
+var bottles = "bottles";
+while (bottles > 0){
+  console.log(bottleNumber + " bottles of beer on the wall,");
+  console.log(bottleNumber + " bottles of beer!");
+  console.log("Take one down and pass it around,");
+  console.log(bottleNumber - 1 + "bottles of beer on the wall!");
+}
